fddcc6c3f25c7934588e0495cadf8ad7cb92093b
”sasaki” <ssk925yuiti@gmail.com>
2020-07-18 07:04:13 +0000

    php課題2

--- practice.php
+++ practice.php
@@ -30,9 +30,49 @@ $calendar_2018 = [
 ];
 
 // 12月を表示する
-echo $calendar_2018["December"];
+echo $calendar_2018["December"]."\n";
 
+echo $name = "佐々木" ."\n";
+/* if文で $name があなたの名前だったら 「私は あなたの名前 です」、
+もし違ったら「あなたの名前ではありません」と表示するように実装してください。*/
+if ($name = "佐々木") {
+  echo "私は　佐々木　です。";
+}else {
+  echo "　佐々木　ではありません。" ."\n";
+}
+// for文を使って、1から10000までの合計の値を表示してください。
+$total = 0; 
+echo $total;
 
+for($i = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total . "\n" ;
+
+//$fruits に配列で好きなフルーツを5個代入し、foreach文で順番に出力してください。
+
+$fruits = array("banana","apple","peach","orange","pine");
+
+echo count($fruits);
+
+foreach($fruits as $fruit){
+  echo "好きなフルーツは" . $fruit;
+  echo "\n";
+}
+
+/* for文の始めの値を定義する */
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i < $end; $i++)
+
+ // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0) {
+  echo $i;
+  }
+  
+  
 
 
 
