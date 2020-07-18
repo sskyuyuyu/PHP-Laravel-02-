0e89981631f8e4a2ca100278ae2410919fcfbd98
”sasaki” <ssk925yuiti@gmail.com>
2020-07-18 00:54:56 +0000

    課題php1

---
+++ practice.php
@@ -0,0 +1,45 @@
+<?php
+echo 'hello php!';
+echo $a = 3;
+echo $b = 7;
+echo $a + $b;
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7]."\n";
+echo $hello = "Hello";
+echo $name = "sasaki";
+echo $world = "s World!";
+echo $hello . $name . $world."\n";
+echo $tech_boost = 'tech';
+echo $tech_boost .= ' boost'."\n";
+echo $tech_boost;
+
+
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+// 12月を表示する
+echo $calendar_2018["December"];
+
+
+
+
+
+
+
+
+
+
+
+
